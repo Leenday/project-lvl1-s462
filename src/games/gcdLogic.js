15691f69b482acc67a81273584e0743e7b46c240
Leenday <iammilagre@gmail.com>
2019-03-20 19:46:13 +0300

    add game gcd

---
+++ src/games/gcdLogic.js
@@ -0,0 +1,27 @@
+import gameRules from '..';
+import { cons } from 'hexlet-pairs';
+import randomIntGenerator from '../functionTools';
+
+const description = 'Find the greatest common divisor of given numbers.\n';
+
+const gcd = () => {
+  const firstNum = randomIntGenerator(2, 60);
+  const secondNum = randomIntGenerator(2, 60);
+  const question = (`${firstNum} ${secondNum}`);
+
+  const rightAnswerGenerator = (firstArg, secondArg) => {
+    const iter = (divider) => {
+      if (firstArg % divider === 0 && secondArg % divider === 0) {
+        return divider;
+      }
+      return iter(divider - 1);
+    };
+    const divider = firstArg > secondArg ? secondArg : firstArg;
+    return iter(divider);
+  };
+  const rightAnswer = String(rightAnswerGenerator(firstNum, secondNum));
+  const gameItSelf = cons(question, rightAnswer);
+  return gameItSelf;
+};
+
+export default () => gameRules(description, gcd);
