041815d995a83f878b99cd7ec4af31a8efb08b64
Leenday <iammilagre@gmail.com>
2019-03-23 00:33:06 +0300

    rename functions

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,25 +4,25 @@ import randomIntGenerator from '../utils';
 
 const description = 'What number is missing in the progression?\n';
 
-const buildPairGameQnA = (num) => {
+const buildPairGameQuestionAnswer = (num) => {
   const step = randomIntGenerator(1, 5);
-  const arrayLength = 10;
-  const indexOfHiddenItem = randomIntGenerator(1, arrayLength - 1);
+  const length = 10;
+  const indexOfHiddenItem = randomIntGenerator(1, length - 1);
   const questionNumbers = [];
-  for (let i = 0; i < arrayLength; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     questionNumbers[i] = num + step * i;
   }
   const rightAnswer = questionNumbers[indexOfHiddenItem];
   questionNumbers[indexOfHiddenItem] = '..';
-  const pairOfGameQnA = cons(rightAnswer, questionNumbers);
-  return pairOfGameQnA;
+  const pairOfGameQuestionAnswer = cons(rightAnswer, questionNumbers);
+  return pairOfGameQuestionAnswer;
 };
 
 const gameData = () => {
-  const quiz = buildPairGameQnA(randomIntGenerator(0, 60));
+  const creationPairQuastionAnswer = buildPairGameQuestionAnswer(randomIntGenerator(0, 60));
 
-  const question = cdr(quiz).join(' ');
-  const rightAnswer = String(car(quiz));
+  const question = cdr(creationPairQuastionAnswer).join(' ');
+  const rightAnswer = String(car(creationPairQuastionAnswer));
   const gameItSelf = cons(question, rightAnswer);
   return gameItSelf;
 };
