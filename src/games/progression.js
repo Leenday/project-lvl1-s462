c3aacd1b85f1bde6a92cd81ca5e4fc6fc2476a87
Leenday <iammilagre@gmail.com>
2019-03-23 16:39:12 +0300

    edit progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,30 +1,29 @@
 import generatorGameData from '..';
-import { cons, car, cdr } from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 import randomIntGenerator from '../utils';
 
 const description = 'What number is missing in the progression?\n';
 
-const buildPairGameQuestionAnswer = (num) => {
+const length = 10;
+
+const buildGameProgression = () => {
+  const start = randomIntGenerator(0, 60);
   const step = randomIntGenerator(1, 5);
-  const length = 10;
-  const indexOfHiddenItem = randomIntGenerator(1, length - 1);
   const questionNumbers = [];
   for (let i = 0; i < length; i += 1) {
-    questionNumbers[i] = num + step * i;
+    questionNumbers[i] = start + step * i;
   }
-  const rightAnswer = questionNumbers[indexOfHiddenItem];
-  questionNumbers[indexOfHiddenItem] = '..';
-  const pairOfGameQuestionAnswer = cons(rightAnswer, questionNumbers);
-  return pairOfGameQuestionAnswer;
+  return questionNumbers;
 };
 
-const gameData = () => {
-  const creationPairQuastionAnswer = buildPairGameQuestionAnswer(randomIntGenerator(0, 60));
-
-  const question = cdr(creationPairQuastionAnswer).join(' ');
-  const rightAnswer = String(car(creationPairQuastionAnswer));
+const generateGameData = () => {
+  const progression = buildGameProgression();
+  const indexOfHiddenItem = randomIntGenerator(1, length - 1);
+  const rightAnswer = String(progression[indexOfHiddenItem]);
+  progression[indexOfHiddenItem] = '..';
+  const question = progression.join(' ');
   const gameItSelf = cons(question, rightAnswer);
   return gameItSelf;
 };
 
-export default () => generatorGameData(description, gameData);
+export default () => generatorGameData(description, generateGameData);
